d40b76d7b57aee2099a0a87fb54b022849beaeeb
Khaleel1911 <work.khaleel.dev@gmail.com>
2024-09-16 14:12:32 +0530

    First version of Slider

---
+++ index.js
@@ -0,0 +1,49 @@
+///Author  : khaleel
+//Date     : 16/09/2024
+// # js
+
+const left=document.getElementById("left");
+const right=document.getElementById("right");
+const imgcontainer=document.getElementById("imgcontainer");
+
+images = ["meta","apple","amazon","netflix","google"];
+
+let cursor=0;
+let intervalid;
+
+// function initializer(){
+//     if(cursor>4){
+//         cursor=0;
+//     }
+//     imgcontainer.innerHTML=`<img src="/images/${images[cursor]}.png">`;
+//     cursor++;
+//     intervalid=setInterval(initializer,5000);
+// }
+
+// initializer();   //option code for initialzing the slider with an automatic sliging option 
+
+left.onclick=function(){
+    clearInterval(intervalid);
+    if(cursor==0)
+    {
+        cursor=4;
+        imgcontainer.innerHTML=`<img src="./images/${images[cursor]}.png">`;
+        // left.disabled="true";
+    }
+    else{
+        cursor--;
+        imgcontainer.innerHTML=`<img src="./images/${images[cursor]}.png">`;
+    }
+}
+right.onclick=function(){
+    if(cursor==4)
+        {
+            cursor=0;
+            imgcontainer.innerHTML=`<img src="./images/${images[cursor]}.png">`;
+        }
+        else{
+            cursor++;
+            imgcontainer.innerHTML=`<img src="./images/${images[cursor]}.png">`;
+        }
+
+}
